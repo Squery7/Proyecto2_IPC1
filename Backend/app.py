d4db2fbaef60a06927e5e7413ec252efa1ce940d
Diego Robles <diegomrza98@gmail.com>
2023-04-17 15:20:33 -0600

    Creacion de pagina base

--- Backend/app.py
+++ Backend/app.py
@@ -7,18 +7,49 @@ CORS(app)
 
 data = data_store()
 
-# \Users\Squery\Desktop\p\myproject\venv\Scripts\activate
-# 
 
 @app.route('/')
 @app.route('/index/')
-@app.route('/index/<name>')
-def hello(name=None):
-    return render_template('index.html', name=name)
+def index():
+    data = {"message": "Index"}
+    return render_template('index.html', data=data)
 
-@app.route('/login')
+
+@app.route('/login/')
 def login():
-    return render_template('login.html')
+    data = {"message": "Login"}
+    return render_template('login.html', data=data)
+
+
+@app.route('/sign_up/')
+def sign_up():
+    data = {"message": "Sign Up"}
+    return render_template('sign_up.html', data=data)
+
+
+@app.route('/catalog/')
+def catalog():
+    data = {"message": "Catalog"}
+    return render_template('catalog.html', data=data)
+
+
+@app.route('/directory/')
+def directory():
+    data = {"message": "Directory"}
+    return render_template('directory.html', data=data)
+
+
+@app.route('/categories/')
+def categories():
+    data = {"message": "Categories"}
+    return render_template('categories.html', data=data)
+
+
+def page_not_found(error):
+    data = {"error": "404", "message": error}
+    return render_template('404.html', data=data), 404
+
 
 if __name__ == "__main__":
+    app.register_error_handler(404, page_not_found)
     app.run(threaded=True, port=5000, debug=True)
